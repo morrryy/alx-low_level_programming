8723d9de34e751f354dbf0739cfc0436fd26ced1
Maryam <morrryyyy20@gmail.com>
2023-02-26 07:04:38 -0800

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints from 0 to 9
+ *
+ * @i: array
+ *
+ * Return: i
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
